a103c2fc14726a8f7b0f57b52ae7166d1b488e39
TonmoyUchiha <tonmoy.uchiha@gmail.com>
2024-12-14 03:04:05 +0600

    Primitive Data Types

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+# Primitive data types in Python
+my_int = 5
+my_str = 'Hello'
+my_float = 3.14
+my_bool = True
+
+print("Integer:", my_int)
+print("String:", my_str)
+print("Float:", my_float)
+print("Boolean:", my_bool)
